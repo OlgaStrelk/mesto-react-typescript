0292fefaad264d9a71c1153abef349a2c5b973eb
“Olga <strelod@yandex.ru>
2024-06-26 20:56:00 +0300

    refactor: header with routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,48 @@
 import React from "react";
-import { Route, Link, Routes } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { PATHS } from "../utils/consts";
+import { useAppSelector } from "../hooks";
 
-function Header({ email, onSignOut }) {
+function Header({ onSignOut }) {
   const [isMenuOpen, setMenuOpen] = React.useState(false);
+  const { _id: email } = useAppSelector((state) => state.user.user);
 
   function toggleMenu() {
     setMenuOpen(!isMenuOpen);
   }
-  let isMain;
-  // const isMain = useRouteMatch({ path: {PATHS.home}, exact: true });
+  const { pathname } = useLocation();
+  const { home, register } = PATHS;
 
+  const isHome = pathname === home;
+  const headerClass = `header  ${isMenuOpen ? "header_menu-open" : ""} 
+    ${isHome ? "header_page-main" : ""}`;
   return (
-    <header
-      className={`header  ${isMenuOpen ? "header_menu-open" : ""} 
-    ${isMain ? "header_page-main" : ""}`}
-    >
+    <header className={headerClass}>
       <div className="header__logo"></div>
-      {/* <Routes>
-        <Route path={PATHS.home}> */}
-      <button
-        className="header__burger"
-        type="button"
-        aria-label="меню"
-        onClick={toggleMenu}
-      ></button>
-      <div className="header__container">
-        <p className="header__user">{email}</p>
-        <button className="header__logout" onClick={onSignOut}>
-          Выйти
-        </button>
-      </div>
-      {/* </Route> */}
-      {/* <Route path={PATHS.register}>
-          <Link className="header__auth-link" to={PATHS.login}>
-            Войти
-          </Link>
-        </Route>
-        <Route path={PATHS.login}>
-          <Link className="header__auth-link" to="sign-up">
-            Регистрация
-          </Link>
-        </Route>
-      </Routes> */}
+      {isHome ? (
+        <>
+          <button
+            className="header__burger"
+            type="button"
+            aria-label="меню"
+            onClick={toggleMenu}
+          ></button>
+          <div className="header__container">
+            <p className="header__user">{email}</p>
+            <button className="header__logout" onClick={onSignOut}>
+              Выйти
+            </button>
+          </div>
+        </>
+      ) : pathname === register ? (
+        <Link className="header__auth-link" to={PATHS.login}>
+          Войти
+        </Link>
+      ) : (
+        <Link className="header__auth-link" to="sign-up">
+          Регистрация
+        </Link>
+      )}
     </header>
   );
 }
